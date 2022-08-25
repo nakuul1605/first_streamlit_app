44a668f9f8c75a9e5fd882954d34e475db3349aa
Nakuul Marwah <112079224+nakuul1605@users.noreply.github.com>
2022-08-25 12:36:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,5 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(my_fruit_list)
+
+streamlit.dataframe(fruits_to_show)
