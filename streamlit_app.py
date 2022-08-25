5186f9abbfcd9af4bb1ed321d3f246f320f85244
Nakuul Marwah <112079224+nakuul1605@users.noreply.github.com>
2022-08-25 12:24:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('In header function')
-streamlit.text('in text function')
-streamlit.text('in text function')
-streamlit.text('in text function')
-streamlit.text('in text function')
-streamlit.text('in text function')
-streamlit.text('in text function')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
