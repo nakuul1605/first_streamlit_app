edef2d04f849b9c65251286d8dc6ca335ba77493
Nakuul Marwah <112079224+nakuul1605@users.noreply.github.com>
2022-08-31 12:43:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,6 @@ try:
 except  URLError as e:
   streamlit.error()
 
-streamlit.stop()
-
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
